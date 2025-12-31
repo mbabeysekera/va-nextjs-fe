44de994c2d89c9f4673bc6ef40f7215332de11fd
babeysekera <revolutiongc.lk@gmail.com>
2026-01-01 01:42:19 +0530

    hotfix: image loading issue fix 2

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "i.ibb.co",
-        port: "",
         pathname: "/**",
       },
     ],
